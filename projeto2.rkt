011d0839e3cc2dd6f19f1bd909063b16ff34a9c1
Jorge Luis <norestraint@protonmail.com>
2024-10-28 16:19:14 -0300

    fix: upcase all constants

--- projeto2.rkt
+++ projeto2.rkt
@@ -82,10 +82,10 @@
 ;;  hpmaximo: Numero, representa a quantia de vida maxima do Pokemon  
 ;;  hpatual: Numero, representa a quantia de vida atual do Pokemon  
 
-(define capturado1 (make-pokemoncapturado POKEMON1 "Pokemon 1" 1 100 90))
-(define capturado2 (make-pokemoncapturado POKEMON2 "Pokemon 2" 2 100 90))
-(define capturado3 (make-pokemoncapturado POKEMON3 "Pokemon 3" 3 100 90))
-(define capturado4 (make-pokemoncapturado POKEMON4 "Pokemon 4" 4 100 90))
+(define CAPTURADO1 (make-pokemoncapturado POKEMON1 "Pokemon 1" 1 100 90))
+(define CAPTURADO2 (make-pokemoncapturado POKEMON2 "Pokemon 2" 2 100 90))
+(define CAPTURADO3 (make-pokemoncapturado POKEMON3 "Pokemon 3" 3 100 90))
+(define CAPTURADO4 (make-pokemoncapturado POKEMON4 "Pokemon 4" 4 100 90))
 
 (define-struct treinador (nome idade vaga1 vaga2 vaga3 vaga4 vaga5 vaga6))
 ;; Um elemento do conjunto Treinador tem o formato (make-treinador nome idade 
@@ -96,10 +96,10 @@
 ;;            treinador possui, ou contem o Symbol 'vazio caso o treinador não 
 ;;            tenha um pokemon naquela vaga.
 
-(define treinador1 (make-treinador "Treinador 1" 18 POKEMON1 POKEMON2 'vazio 'vazio POKEMON4 'vazio))
-(define treinador2 (make-treinador "Treinador 2" 19 'vazio POKEMON2 'vazio 'vazio POKEMON4 'vazio))
-(define treinador3 (make-treinador "Treinador 3" 20 'vazio 'vazio 'vazio POKEMON3 POKEMON4 'vazio))
-(define treinador4 (make-treinador "Treinador 4" 21 'vazio 'vazio 'vazio 'vazio POKEMON4 'vazio))
+(define TREINADOR1 (make-treinador "Treinador 1" 18 POKEMON1 POKEMON2 'vazio 'vazio POKEMON4 'vazio))
+(define TREINADOR2 (make-treinador "Treinador 2" 19 'vazio POKEMON2 'vazio 'vazio POKEMON4 'vazio))
+(define TREINADOR3 (make-treinador "Treinador 3" 20 'vazio 'vazio 'vazio POKEMON3 POKEMON4 'vazio))
+(define TREINADOR4 (make-treinador "Treinador 4" 21 'vazio 'vazio 'vazio 'vazio POKEMON4 'vazio))
 
 ;;##########################################################################
 ;;### 2 ### 2 ### 2 ### 2 ### 2 ### 2 ### 2 ### 2 ### 2 ### 2 ### 2    #####
